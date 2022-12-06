1df94b2ff2c240e9360c588511db3d68d56412d9
EmilianoUT <48842038+EmilianoUT@users.noreply.github.com>
2022-12-05 20:15:29 -0600

    CACHE FUNCIONANADO

--- sw.js
+++ sw.js
@@ -1,62 +1,90 @@
-const CACHE_NAME = 'v1.cache.awp';
-var urlToCache = [
-    './', 
-    './style.css', 
-    './assets/Earthbound1.jpg',
-    './assets/la-saga-de-earthbound-conocida-.gif',
-    './assets/logo.png',
-    './assets/Mother1.jpg',
-    './assets/mother3.jpg',
-    './assets/onettnight.png',
-    './assets/Earthbound.tff',
-]
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open("v1");
+    await cache.addAll(resources);
+};
 
-self.addEventListener('install', e => {
-    e.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(cache => {
-            return cache.addAll(urlToCache)
-            .then(() => {
-                self.skipWaiting();
-            })
-        })
-        .catch(err => {
-            console.log("NO SE HA RESGUARDADO EL CACHE", err)
-        })
-    )
-})
+const putInCache = async (request, response) => {
+    const cache = await caches.open("v1");
+    await cache.put(request, response);
+};
 
-// event activate
-self.addEventListener('activate', e => {
-    const cacheWhiteList = [CACHE_NAME];
-    e.waitUntil(
-        caches.keys()
-        .then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if(cacheWhiteList.indexOf(cacheName) === - 1){
-                        // borrar los elementos que no necesita
-                        return caches.delete(cacheName);
-                    }
-                })
-            )
-        })
-        .then(() => {
-            // activar la cache
-            self.clients.claim();
-        })
-    )
-})
+const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
+    // First try to get the resource from the cache
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+        return responseFromCache;
+    }
 
-// evento fetch
-self.addEventListener('fetch', e =>{
-    e.respondWith(
-        caches.match(e.request)
-        .then(res => {
-            if(res){
-                return res;
-            }
-            return fetch(e.request);
+    // Next try to use (and cache) the preloaded response, if it's there
+    const preloadResponse = await preloadResponsePromise;
+    if (preloadResponse) {
+        console.info("using preload response", preloadResponse);
+        putInCache(request, preloadResponse.clone());
+        return preloadResponse;
+    }
+
+    // Next try to get the resource from the network
+    try {
+        const responseFromNetwork = await fetch(request);
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
+        putInCache(request, responseFromNetwork.clone());
+        return responseFromNetwork;
+    } catch (error) {
+        const fallbackResponse = await caches.match(fallbackUrl);
+        if (fallbackResponse) {
+            return fallbackResponse;
+        }
+        // when even the fallback response is not available,
+        // there is nothing we can do, but we must always
+        // return a Response object
+        return new Response("Network error happened", {
+            status: 408,
+            headers: { "Content-Type": "text/plain" },
+        });
+    }
+};
+
+// Enable navigation preload
+const enableNavigationPreload = async () => {
+    if (self.registration.navigationPreload) {
+        // Enable navigation preloads!
+        await self.registration.navigationPreload.enable();
+    }
+};
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(enableNavigationPreload());
+});
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        addResourcesToCache([
+            '/',
+
+            'index.html',
+            'main.js',
+            'manifest.json',
+            'style.css',
+            'sw.js',
+            'assets/Earthbound1.jpg',
+            'assets/EB.gif',
+            'assets/logo.png',
+            'assets/Mother1.jpg',
+            'assets/mother3.jpg',
+            'assets/onettnight.png',
+            'assets/Earthbound.tff',
+        ])
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        cacheFirst({
+            request: event.request,
+            preloadResponsePromise: event.preloadResponse,
+            fallbackUrl: "/assets/Mother1.jpg",
         })
-    )
-})
+    );
+});
